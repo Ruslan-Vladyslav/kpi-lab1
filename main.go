95868b63741fa1b9fa84b81b385151c3d5255209
Ruslan-Vladyslav <rgritsai95@gmail.com>
2025-05-18 22:48:30 +0300

    Changed to english

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
+	"kpi-lab1/server"
 	"log"
 	"net/http"
-	"kpi-lab1/server"
 )
 
 func main() {
 	http.HandleFunc("/time", server.TimeHandler)
 
-	log.Println("Сервер запущен на http://localhost:8795")
+	log.Println("Server is running on http://localhost:8795")
 	err := http.ListenAndServe(":8795", nil)
 	if err != nil {
 		log.Fatal(err)
