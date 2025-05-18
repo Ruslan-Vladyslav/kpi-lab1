54654ad76acd16c88f9313bcf85567306f9b52db
Ruslan-Vladyslav <rgritsai95@gmail.com>
2025-05-18 23:00:43 +0300

    Added messages for console

--- server/time.go
+++ server/time.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 	"time"
 )
@@ -11,9 +12,14 @@ type TimeResponse struct {
 }
 
 func TimeHandler(w http.ResponseWriter, r *http.Request) {
+	currentTime := time.Now().Format(time.RFC3339)
+
+	log.Printf("GET /time -> %s\n", currentTime)
+
 	response := TimeResponse{
-		Time: time.Now().Format(time.RFC3339),
+		Time: currentTime,
 	}
+
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(response)
 }
