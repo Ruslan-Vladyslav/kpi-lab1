1e6ffc71e03011b888628f2a6d94eb462e5c928e
KuHavoc <dmitroivanyta@gmail.com>
2025-05-20 19:30:04 +0300

    Add comments time.go

--- server/time.go
+++ server/time.go
@@ -12,18 +12,23 @@ type TimeResponse struct {
 }
 
 func TimeHandler(w http.ResponseWriter, r *http.Request) {
+	// Use RFC3339 format
 	currentTime := time.Now().Format(time.RFC3339)
 
+	// Log req
 	log.Printf("GET /time -> %s\n", currentTime)
-
 	log.Printf("Request from %s to %s", r.RemoteAddr, r.URL.Path)
 
+	// Get current time
 	response := TimeResponse{
 		Time: currentTime,
 	}
 
+	// Set header
 	w.Header().Set("Content-Type", "application/json")
 	err := json.NewEncoder(w).Encode(response)
+
+	// Handle error
 	if err != nil {
 		return
 	}
