350b3988f54dca100dc7c5a3bd657906d2fbdb20
KuHavoc <dmitroivanyta@gmail.com>
2025-05-20 19:28:28 +0300

    Add comments main.go

--- main.go
+++ main.go
@@ -9,8 +9,13 @@ import (
 func main() {
 	http.HandleFunc("/time", server.TimeHandler)
 
+	// Log
 	log.Println("Server is running on http://localhost:8795")
+
+	// Start
 	err := http.ListenAndServe(":8795", nil)
+
+	// If Error -- log
 	if err != nil {
 		log.Fatal(err)
 	}
